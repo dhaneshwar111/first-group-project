b0344d28958e6bde795674263c6360608a392b00
dhaneshwar111 <dhaneshwarkumar435@gmail.com>
2023-11-18 01:23:31 +0530

    remove one page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from './Header/header';
 import './App.css';
-import NewPages from './NewPages';
+
 
 function App() {
   return (
     <div className="App">
      <Header/>
-     <NewPages/>
+    
     </div>
   );
 }
--- src/NewPages.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NewPages = () => {
-  return (
-    <div>NewPages</div>
-  )
-}
-
-export default NewPages
